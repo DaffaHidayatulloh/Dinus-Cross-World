4711d79294887e20380455183b49d3386c1155bc
Subekti <daffahidayatulah123@gmail.com>
2024-04-03 13:50:48 +0700

    Fix Bug Nembus Badan

--- Dinus Cross World/Assets/Script/NPC Follow.cs
+++ Dinus Cross World/Assets/Script/NPC Follow.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCFollow : MonoBehaviour
+
 {
     public Transform target; // Target yang akan diikuti (misalnya karakter yang dapat dimainkan)
     public float moveSpeed = 3f; // Kecepatan gerak NPC
     public float stoppingDistance = 2f; // Jarak di mana NPC berhenti
+    public BoxCollider2D npcCollider; // Collider NPC
 
     private Rigidbody2D rb;
     private bool isFollowing = true;
+    private bool isPassable = false; // Apakah NPC dapat dilewati saat karakter di atasnya
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // Membuat collider NPC tidak aktif saat awal permainan
+        npcCollider.enabled = false;
     }
 
     private void Update()
@@ -42,5 +47,31 @@ public class NPCFollow : MonoBehaviour
     public void SetFollowing(bool follow)
     {
         isFollowing = follow;
+    }
+
+    // Method untuk mengatur apakah NPC dapat dilewati atau tidak
+    public void SetPassable(bool passable)
+    {
+        isPassable = passable;
+        // Mengaktifkan atau menonaktifkan collider NPC berdasarkan nilai isPassable
+        npcCollider.enabled = !passable;
+    }
+
+    // Ketika karakter berada di atas NPC, NPC akan dilewati
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SetPassable(true);
+        }
+    }
+
+    // Ketika karakter meninggalkan area di atas NPC, NPC tidak akan dilewati lagi
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SetPassable(false);
+        }
     }
 }
