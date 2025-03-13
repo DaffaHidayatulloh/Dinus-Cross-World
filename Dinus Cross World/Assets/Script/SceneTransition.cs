651f753194dc4df04efb21f1f6418ded33706a25
Subekti <daffahidayatulah123@gmail.com>
2025-03-13 15:37:09 +0700

    Add Shadow Map Galeri

---
+++ Dinus Cross World/Assets/Script/SceneTransition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+   
+    public string sceneName; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            SceneManager.LoadScene(sceneName); 
+        }
+    }
+}
+
